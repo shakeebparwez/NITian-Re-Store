1bbd23a7a805faa4d0275081b00b384e058497a2
Shakeeb Parwez <shakeebparwez@gmail.com>
2024-03-05 00:05:41 +0530

    Create Login API

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // new user registration
 router.post("/register", async (req, res) => {
@@ -29,4 +30,43 @@ router.post("/register", async (req, res) => {
         message: error.message,
       });
     }
-  });
+});
+
+// user login
+router.post("/login", async (req, res) => {
+    try {
+      // check if user exists
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      // compare password
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!validPassword) {
+        throw new Error("Invalid password");
+      }
+  
+      // create and assign token
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+  
+      // send response
+      res.send({
+        success: true,
+        message: "User logged in successfully",
+        data: token,
+      });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,4 +5,10 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 
+app.use(express.json());
+
+const usersRoute = require('./routes/usersRoute');
+
+app.use('/api/users', usersRoute);
+
 app.listen(port, () => console.log(`Node/Express Server Started on Port ${port}`));
