a95ad8f9fceb677d4cf0b6dd6182b6f87dff51c4
kamal9732 <kamal9732.kkr@gmail.com>
2024-03-07 01:01:00 +0530

    Add Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
+import ProtectedPage from "./components/ProtectedPage";
 
 function App() {
   return (
     <div>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/" element={<ProtectedPage><Home /></ProtectedPage>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
           </Routes>
---
+++ client/src/components/ProtectedPage.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { message } from "antd";
+import { useNavigate } from "react-router-dom";
+import { GetCurrentUser } from "../apicalls/users";
+
+function ProtectedPage({children}) {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  
+  const validateToken = async () => {
+    try {
+      const response = await GetCurrentUser();
+
+      if (response.success) {
+        setUser(response.data);
+      } else {
+        navigate("/login");
+        message.error(response.message);
+      }
+    } catch (error) {
+      navigate("/login");
+      message.error(error.message);
+    }
+  };
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+        validateToken();
+      } else {
+        navigate("/login");
+      }
+  }, []);
+  
+  return (
+    <div>{user && 
+        <div className="p-5">
+            {user.name}
+            {children}
+        </div>
+    }</div>
+  )
+}
+
+export default ProtectedPage
