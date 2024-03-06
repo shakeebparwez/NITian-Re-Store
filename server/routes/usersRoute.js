60a56706a679e8cc45c80815de0a1c24102ef99e
kamal9732 <kamal9732.kkr@gmail.com>
2024-03-07 00:11:42 +0530

    Create Authorization Middlewares

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlwares/authMiddleware");
 
 // new user registration
 router.post("/register", async (req, res) => {
@@ -67,6 +68,23 @@ router.post("/login", async (req, res) => {
         message: error.message,
       });
     }
+});
+
+// get current user
+router.get("/get-current-user", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    res.send({
+      success: true,
+      message: "User fetched successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
 module.exports = router;
