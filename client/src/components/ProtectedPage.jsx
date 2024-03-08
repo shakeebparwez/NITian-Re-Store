e5aafe965f6ced4425b8b90136d4867efae1c741
kamal9732 <kamal9732.kkr@gmail.com>
2024-03-08 22:48:09 +0530

    Create Default Layout

--- client/src/components/ProtectedPage.jsx
+++ client/src/components/ProtectedPage.jsx
@@ -3,10 +3,10 @@ import { message } from "antd";
 import { useNavigate } from "react-router-dom";
 import { GetCurrentUser } from "../apicalls/users";
 
-function ProtectedPage({children}) {
+function ProtectedPage({ children }) {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
-  
+
   const validateToken = async () => {
     try {
       const response = await GetCurrentUser();
@@ -24,20 +24,42 @@ function ProtectedPage({children}) {
   };
   useEffect(() => {
     if (localStorage.getItem("token")) {
-        validateToken();
-      } else {
-        navigate("/login");
-      }
+      validateToken();
+    } else {
+      navigate("/login");
+    }
   }, []);
-  
+
   return (
-    <div>{user && 
-        <div className="p-5">
-            {user.name}
-            {children}
+    user && (
+      <div>
+        {/* header */}
+        <div className="flex justify-between items-center bg-primary p-5">
+          <h1
+            className="text-2xl text-white cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            NITian ReStore
+          </h1>
+
+          <div className="bg-white py-2 px-5 rounded flex gap-1 items-center">
+            <i class="ri-shield-user-line"></i>
+            <span className="underline cursor-pointer">{user.name}</span>
+            <i
+              className="ri-logout-box-r-line ml-10"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login");
+              }}
+            ></i>
+          </div>
         </div>
-    }</div>
-  )
+
+        {/* body */}
+        <div className="p-5">{children}</div>
+      </div>
+    )
+  );
 }
 
-export default ProtectedPage
+export default ProtectedPage;
