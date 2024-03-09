dd933d343e792d81a63c94f9562ce0609acc7857
Shakeeb Parwez <shakeebparwez@gmail.com>
2024-03-09 23:20:26 +0530

    Redux Setup - Create Store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,25 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ConfigProvider } from 'antd'
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ConfigProvider 
-      theme={{
-        components : {
-          Button : {
-            colorPrimary : '#CF2E2E',
-            colorPrimaryHover : '#CF2E2E',
-            borderRadius : '2px',
-            boxShadow : 'none',
-          }
-        },
-        token : {
-          borderRadius : '2px',
-          colorPrimary : '#CF2E2E',
-        },
-    }}>
-      <App />
-    </ConfigProvider>
-  </React.StrictMode>,
+    <Provider store={store}>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              colorPrimary: '#CF2E2E',
+              colorPrimaryHover: '#CF2E2E',
+              borderRadius: '2px',
+              boxShadow: 'none',
+            }
+          },
+          token: {
+            borderRadius: '2px',
+            colorPrimary: '#CF2E2E',
+          },
+        }}>
+        <App />
+      </ConfigProvider>
+    </Provider>
 )
