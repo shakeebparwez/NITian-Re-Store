a5e5105d8915453fdb07e442282d651b4b929db0
Shakeeb Parwez <shakeebparwez@gmail.com>
2024-03-23 14:53:54 +0530

    Create Products Table

--- client/src/pages/Profile/Products/Products.jsx
+++ client/src/pages/Profile/Products/Products.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
-import { Button } from "antd";
+import React, { useEffect } from "react";
+import { Button, message, Table } from "antd";
 import ProductsForm from "./ProductsForm";
+import { useDispatch, useSelector } from "react-redux";
+import { SetLoader } from "../../../redux/loadersSlice";
+import { GetProducts } from "../../../apicalls/products";
+
 function Products() {
+  const [products, setProducts] = React.useState([]);
   const [showProductForm, setShowProductForm] = React.useState(false);
+  const dispatch = useDispatch();
+
+  const getData = async () => {
+    try {
+      dispatch(SetLoader(true));
+      const response = await GetProducts();
+      dispatch(SetLoader(false));
+      if (response.success) {
+        setProducts(response.data);
+      }
+    } catch (error) {
+      dispatch(SetLoader(false));
+      message.error(error.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: (text, record) => {
+        return (
+          <div className="flex gap-5 items-center">
+            <i
+              className="ri-delete-bin-line"
+            ></i>
+            <i
+              className="ri-pencil-line"
+            ></i>
+          </div>
+        );
+      }
+    },
+  ];
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <div className="flex justify-end mb-2">
@@ -14,7 +78,7 @@ function Products() {
           Add Product
         </Button>
       </div>
-
+      <Table columns={columns} dataSource={products} />
       {showProductForm && (
         <ProductsForm
           showProductForm={showProductForm}
