d9fb7bda6353647697411719ad37e8a96dc990a9
Shakeeb Parwez <shakeebparwez@gmail.com>
2024-03-23 14:19:19 +0530

    Create Product APIs

--- server/server.js
+++ server/server.js
@@ -8,10 +8,12 @@ const dbConfig = require('./config/dbConfig');
 app.use(express.json());
 
 const usersRoute = require('./routes/usersRoute');
+const productsRoute = require('./routes/productsRoute');
 
 const cors = require('cors');
 app.use(cors());
 
 app.use('/api/users', usersRoute);
+app.use('/api/products', productsRoute);
 
 app.listen(port, () => console.log(`Node/Express Server Started on Port ${port}`));
