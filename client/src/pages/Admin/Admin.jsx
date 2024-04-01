b9f39d987b9a32d4abee744a4cc6c9f8b67e37d2
Shakeeb Parwez <shakeebparwez@gmail.com>
2024-04-01 13:12:52 +0530

    Users List and Users Status Update

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Tabs } from "antd";
 import Products from "./Products";
+import Users from "./Users";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Admin() {
+    const navigate = useNavigate();
+    const { user } = useSelector((state) => state.users);
+    useEffect(() => {
+      if (user.role !== "admin") {
+        navigate("/");
+      }
+    }, []);
     return (
         <div>
             <Tabs>
@@ -10,7 +20,7 @@ function Admin() {
                     <Products />
                 </Tabs.TabPane>
                 <Tabs.TabPane tab="Users" key="2">
-                    <h1>Users</h1>
+                    <Users />
                 </Tabs.TabPane>
             </Tabs>
         </div>
