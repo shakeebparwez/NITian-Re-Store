33b70e24b2fd9530ac00f26b5f44096e60cf6f81
Shakeeb Parwez <shakeebparwez@gmail.com>
2024-04-02 11:48:10 +0530

    Create Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import ProtectedPage from "./components/ProtectedPage";
+import ProductInfo from "./pages/ProductInfo/ProductInfo";
 import Spinner from "./components/Spinner";
 import Profile from "./pages/Profile/Profile";
 import Admin from "./pages/Admin/Admin";
@@ -12,16 +13,17 @@ function App() {
   const { loading } = useSelector((state) => state.loaders);
   return (
     <div>
-        {loading && <Spinner />}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<ProtectedPage><Home /></ProtectedPage>} />
-            <Route path="/profile" element={<ProtectedPage><Profile /></ProtectedPage>} />
-            <Route path="/admin" element={<ProtectedPage><Admin /></ProtectedPage>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
-          </Routes>
-        </BrowserRouter>
+      {loading && <Spinner />}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProtectedPage><Home /></ProtectedPage>} />
+          <Route path="/product/:id" element={<ProtectedPage><ProductInfo /></ProtectedPage>} />
+          <Route path="/profile" element={<ProtectedPage><Profile /></ProtectedPage>} />
+          <Route path="/admin" element={<ProtectedPage><Admin /></ProtectedPage>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,15 +1,75 @@
-import React from 'react'
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { GetProducts } from "../../apicalls/products";
+import { SetLoader } from "../../redux/loadersSlice";
+import { message } from "antd";
+import Divider from "../../components/Divider";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const [showFilters, setShowFilters] = React.useState(true);
+  const [products, setProducts] = React.useState([]);
+  const [filters, setFilters] = React.useState({
+    status: "approved",
+  });
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.users);
 
+  const getData = async () => {
+    try {
+      dispatch(SetLoader(true));
+      const response = await GetProducts(filters);
+      dispatch(SetLoader(false));
+      if (response.success) {
+        setProducts(response.data);
+      }
+    } catch (error) {
+      dispatch(SetLoader(false));
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [filters]);
+
   return (
-    <div>
-      <h1>Home</h1>
-      {user && <h1>{user.name}</h1>}
+    <div
+      className={`
+        grid gap-5 ${showFilters ? "grid-cols-4" : "grid-cols-5"}
+      `}
+    >
+      {products?.map((product) => {
+        return (
+          <div
+            className="border border-gray-300 rounded border-solid flex flex-col gap-2 pb-2 cursor-pointer"
+            key={product._id}
+            onClick={() => navigate(`/product/${product._id}`)}
+          >
+            <img
+              src={product.images[0]}
+              className="w-full h-52 p-2 rounded-md object-cover"
+              alt=""
+            />
+            <div className="px-2 flex flex-col">
+              <h1 className="text-lg font-semibold">{product.name}</h1>
+              <p className="text-sm">
+                {product.age} {' '}
+                {product.age === 1 ? " year" : " years"} {' '}
+                old
+              </p>
+              <Divider />
+              <span className="text-xl font-semibold text-green-700">
+                ₹ {product.price}
+              </span>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
